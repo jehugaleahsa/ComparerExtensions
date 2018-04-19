a151266a738ef9b916c4e84563aeedd4adf51fa0
jehugaleahsa <jehugaleahsa@gmail.com>
2018-04-19 10:58:28 -0400

    Update deployment scripts

--- Deployment/publish-comparer-extensions.ps1
+++ Deployment/publish-comparer-extensions.ps1
@@ -1,2 +1,5 @@
+&dotnet pack "..\ComparerExtensions\ComparerExtensions.csproj" --configuration Release --output $PWD
 
+.\NuGet.exe push ComparerExtensions.*.nupkg -Source https://www.nuget.org/api/v2/package
 
+Remove-Item ComparerExtensions.*.nupkg
